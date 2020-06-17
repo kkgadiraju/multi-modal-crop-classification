d024a003c90caacc790d14fee3bc4706f8da7ec3
Krishna Karthik Gadiraju <kkgadiraju@gmail.com>
2020-06-17 13:09:17 -0400

    remove old files

--- bilstm/grid_search.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-source activate cnn-rnn
-#python train-simple-spatiotemporal.py -c './config.ini' -t 'spatiotemporal' -d $1 -r $2
-python train-gridsearch.py -c './config-gs.ini' -t 'temporal' 
--- bilstm/run_train.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-source activate cnn-rnn
-python train.py -c './config.ini' -t 'spatiotemporal' 
